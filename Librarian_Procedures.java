74269f9025c39dc24befbf4a7771bac4f0555337
Jean-Paul Kalenderian <jpkalenderian@gmail.com>
2024-03-23 19:45:29 +0200

    Created the Librarian_Procedures Interface

---
+++ Librarian_Procedures.java
@@ -0,0 +1,7 @@
+public interface Librarian_Procedures {
+    public void searchBookByTitle(String title);
+    public void searchBookByAuthor(String name);
+    public void searchBookByGenre(String genre);
+    public void addUser();
+    public void addBooks();
+}
